0c67351b3e80bc07c30f14f126450e7619bb0b93
Himavarshith Reddy <himavarshithreddy@gmail.com>
2023-11-23 09:31:09 +0530

    Update weatherappp.jsx

--- src/weatherappp.jsx
+++ src/weatherappp.jsx
@@ -138,6 +138,7 @@ function Weatherapp(){
     const fetchUserLocation = async () => {
       try {
         // Fetch user's IP address
+        
         const response = await fetch('https://api.ipdata.co?api-key=cb980b4d080e774050e182dfea975621bfb35dbdebce8c88ba847baf');
         const data = await response.json();
   
